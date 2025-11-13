dd3a9fe4d60d433e21e876cd7a9d00a3f11bc77b
sandersvendgard <sander.svendgard@icloud.com>
2025-11-13 14:30:41 +0100

    maindddd

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+
+void setup() {
+  Serial.begin(115200);
+  delay(300);
+  Serial.println("\nHello from ESP32!");
+}
+
+void loop() {
+  Serial.println("loop tick");
+  delay(1000);
+}
